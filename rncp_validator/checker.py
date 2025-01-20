55f3e80230dd0fafddac18393bd33cea50645f89
farinaleo <leo.farina.fr@gmail.com>
2025-01-20 19:59:12 +0100

    Add tho option to select a  branch and list all possible branches if the option doesnt exist

--- rncp_validator/checker.py
+++ rncp_validator/checker.py
@@ -8,17 +8,25 @@ from openpyxl import load_workbook
 from rncp_validator.Calendar import Calendar
 
 
-def get_all_commits(git_path: str) -> dict:
+def get_all_commits(git_path: str, branch: str = None) -> dict:
     """
     Get from the given git path all the commits from the head.
     :param git_path: The .git path.
+    :param branch: The branch to check.
     :return: All commits as a dict the hexa as key and the date as value.
     """
     commits = dict()
 
     repo = Repo(git_path)
-    print(f"Working on all commits from {repo.active_branch.name}...")
-    for commit in repo.iter_commits(repo.active_branch.name):
+
+    if branch not in repo.branches:
+        raise ValueError(
+            f"Branch '{branch}' does not exist in the repository."
+            + f" Possible choice {[branch.name for branch in repo.branches]}"
+        )
+    print(f"Working on all commits from {repo.active_branch.name if not branch else branch}...")
+
+    for commit in repo.iter_commits(repo.active_branch.name if not branch else branch):
         commits[commit.hexsha] = commit.committed_datetime
 
     return commits
@@ -34,13 +42,14 @@ def main():
         parser = argparse.ArgumentParser(description="Process calendar and git parse arguments.")
         parser.add_argument("calendar_path", type=str, help="Path to the calendar file")
         parser.add_argument("git_parse", type=str, help="Git parse argument")
+        parser.add_argument("--branch", type=str, default=None, help="Branch to check")
 
         args = parser.parse_args()
 
         cl = Calendar(load_workbook(args.calendar_path))
         cl.get_periods()
 
-        commit_list = get_all_commits(args.git_parse)
+        commit_list = get_all_commits(args.git_parse, branch=args.branch)
 
         for commits in commit_list:
             if cl.date_in_period(commit_list[commits]):
